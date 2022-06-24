63db99e326d33e95b78824757d85f7de411011bb
pguyon <pguyon69@gmail.com>
2022-06-24 10:01:25 +0200

    fix bug on filter input

--- scripts/search.js
+++ scripts/search.js
@@ -1,12 +1,10 @@
-const result = document.getElementById("main__search");
-const recipeGallery = document.querySelector(".recipe__gallery");
-
 //Searchbar function
 
 const mainSearch = () => {
   let filteredRecipes = recipes;
+  const result = document.getElementById("main__search").value.toLowerCase();
 
-  // Tag filters - Secondary Search
+  // Tag filters
   document.querySelectorAll("#resume__filter a").forEach((tag) => {
     const tagInput = tag.textContent.toLocaleLowerCase();
     filteredRecipes = filteredRecipes.filter(
@@ -23,37 +21,32 @@ const mainSearch = () => {
   });
 
   // SearchBar
-  result.addEventListener("keyup", (e) => {
-    let inputValue = e.target.value.toLowerCase();
-    if (inputValue.length >= 3) {
-      const newRecipeTable = [];
-      for (let i = 0; i < filteredRecipes.length; i++) {
-        const food = filteredRecipes[i];
-        let ingredientsIncluded = false;
-        for (let j = 0; j < food.ingredients.length; j++) {
-          if (
-            food.ingredients[j].ingredient.toLowerCase().includes(inputValue)
-          ) {
-            ingredientsIncluded = true;
-          }
-        }
-        if (
-          food.name.toLowerCase().includes(inputValue) ||
-          food.description.toLowerCase().includes(inputValue) ||
-          ingredientsIncluded
-        ) {
-          newRecipeTable.push(food);
-        }
-      }
 
-      if (filteredRecipes.length === 0) {
-        recipeGallery.textContent =
-          "Aucune recette ne correspond à vos critères";
-      } else {
-        updateContent(newRecipeTable);
+  if (result.length >= 3) {
+    const newRecipeTable = [];
+    for (let i = 0; i < filteredRecipes.length; i++) {
+      const food = filteredRecipes[i];
+      let ingredientsIncluded = false;
+      for (let j = 0; j < food.ingredients.length; j++) {
+        if (food.ingredients[j].ingredient.toLowerCase().includes(result)) {
+          ingredientsIncluded = true;
+        }
       }
-    } else {
-      updateContent(filteredRecipes);
+      if (
+        food.name.toLowerCase().includes(result) ||
+        food.description.toLowerCase().includes(result) ||
+        ingredientsIncluded
+      ) {
+        newRecipeTable.push(food);
+      }
     }
-  });
+
+    if (filteredRecipes.length === 0) {
+      recipeGallery.textContent = "Aucune recette ne correspond à vos critères";
+    } else {
+      updateContent(newRecipeTable);
+    }
+  } else {
+    updateContent(filteredRecipes);
+  }
 };
