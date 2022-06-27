066186b64f33428f6e8bfe1dd5e26c9422242874
pguyon <pguyon69@gmail.com>
2022-06-27 08:27:47 +0200

    start array method on search bar

--- scripts/search.js
+++ scripts/search.js
@@ -22,33 +22,19 @@ const mainSearch = () => {
   });
 
   // SearchBar
-
   if (result.length >= 3) {
-    const newRecipeArray = [];
-    for (let i = 0; i < filteredRecipes.length; i++) {
-      const food = filteredRecipes[i];
-      let ingredientsIncluded = false;
-      for (let j = 0; j < food.ingredients.length; j++) {
-        if (food.ingredients[j].ingredient.toLowerCase().includes(result)) {
-          ingredientsIncluded = true;
-        }
-      }
-      if (
-        food.name.toLowerCase().includes(result) ||
-        food.description.toLowerCase().includes(result) ||
-        ingredientsIncluded
-      ) {
-        newRecipeArray.push(food);
-      }
-    }
-
+    filteredRecipes = filteredRecipes.filter((recipe) => {
+      recipe.description.toLowerCase().includes(result) ||
+        recipe.name.toLowerCase().includes(result) ||
+        recipe.ingredients.some((i) =>
+          i.ingredient.toLowerCase().includes(result)
+        );
+    });
     if (filteredRecipes.length === 0) {
-      console.log("tableau vide");
       document.querySelector(".recipe__gallery").textContent =
         "Aucunes recettes ne correspond à vos critères";
     } else {
       updateContent(newRecipeArray);
-      console.log("je sais pas où est le problème");
     }
   } else {
     updateContent(filteredRecipes);
